ebe859e7005cc9c287b55c9c3d7aa3d1e51e61c6
Ricardo Amaro <ramaro@google.com>
2017-10-23 16:34:21 +0100

    fix package_data glob in setup.py

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     keywords='jsonnet kubernetes reclass jinja',
     py_modules=["kapitan"],
     packages=find_packages(),
-    package_data={"": ["lib/"]},
+    package_data={"": ["lib/*"]},
     include_package_data=True,
     install_requires=[
         'jsonnet>=0.9.5',
