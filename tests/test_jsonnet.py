6235985f886a42d9fadb11a6a9bf78880452d130
Ricardo Amaro <ramaro@google.com>
2017-12-15 17:25:27 +0000

    new yaml_dump() native jsonnet function

---
+++ tests/test_jsonnet.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#
+# Copyright 2017 The Kapitan Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"jsonnet tests"
+import unittest
+from kapitan.resources import yaml_dump
+
+class JsonnetNativeFuncsTest(unittest.TestCase):
+    def test_yaml_dump(self):
+        "dump json string to yaml"
+        yaml = yaml_dump("{\"key\":\"value\"}")
+        self.assertEqual(yaml, "key: value\n")
