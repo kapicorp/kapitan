4494cc04009bf7684b3fc12ed8fc7ee2fd9dc0de
Adrian Chifor <chifor@google.com>
2018-03-22 16:41:21 +0000

    Updated copyright

--- examples/kubernetes/scripts/kubectl.sh
+++ examples/kubernetes/scripts/kubectl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 #
-# Copyright 2017 The Kapitan Authors
+# Copyright 2018 The Kapitan Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
