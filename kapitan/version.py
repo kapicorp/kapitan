263404a90dacd84d4281be777c53bb2322f94223
Adrian Chifor <chifor@google.com>
2018-05-31 17:59:38 +0100

    Version incremenet 0.16.8

--- kapitan/version.py
+++ kapitan/version.py
@@ -17,7 +17,7 @@
 "Project description variables"
 
 PROJECT_NAME = 'kapitan'
-VERSION = '0.16.5'
+VERSION = '0.16.8'
 DESCRIPTION = ('Generic templated configuration management for Kubernetes, '
                'Terraform and other things')
 AUTHOR = 'Ricardo Amaro'
