3f8a54658fb5dd16a6e8b724befa28e0d8f65494
Luis Buriola <buriola@google.com>
2018-11-01 14:43:24 +0000

    Version increment 0.19.0

--- kapitan/version.py
+++ kapitan/version.py
@@ -17,7 +17,7 @@
 "Project description variables"
 
 PROJECT_NAME = 'kapitan'
-VERSION = '0.18.2'
+VERSION = '0.19.0'
 DESCRIPTION = ('Generic templated configuration management for Kubernetes, '
                'Terraform and other things')
 AUTHOR = 'Ricardo Amaro'
