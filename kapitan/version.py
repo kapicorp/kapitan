2621d2cdf2da46d9d72c5393bad0904a47b0e44b
Kapitan CI <kapitan@google.com>
2019-01-09 10:37:46 +0000

    Version increment 0.22.0

--- kapitan/version.py
+++ kapitan/version.py
@@ -17,7 +17,7 @@
 "Project description variables"
 
 PROJECT_NAME = 'kapitan'
-VERSION = '0.22.0-rc.5'
+VERSION = '0.22.0'
 DESCRIPTION = ('Generic templated configuration management for Kubernetes, '
                'Terraform and other things')
 AUTHOR = 'Ricardo Amaro'
