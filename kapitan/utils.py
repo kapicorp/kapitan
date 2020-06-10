a45051bcbf44c1e2aa09ebed93f298bbe300601b
uberspot <onexemailx@gmail.com>
2020-06-10 18:02:27 +0100

    Allow using .yaml output file extension along with .yml and start release-v0.28.0-rc.1

--- kapitan/utils.py
+++ kapitan/utils.py
@@ -24,7 +24,8 @@ import requests
 import yaml
 from kapitan import cached, defaults
 from kapitan.errors import CompileError
-from kapitan.inputs.jinja2_filters import load_jinja2_filters, load_jinja2_filters_from_file
+from kapitan.inputs.jinja2_filters import (load_jinja2_filters,
+                                           load_jinja2_filters_from_file)
 from kapitan.version import VERSION
 
 "random utils"
@@ -211,7 +212,7 @@ def flatten_dict(d, parent_key="", sep="."):
     items = []
     for k, v in d.items():
         new_key = parent_key + sep + k if parent_key else k
-        if isinstance(v, collections.MutableMapping):
+        if isinstance(v, collections.abc.MutableMapping):
             items.extend(flatten_dict(v, new_key, sep=sep).items())
         else:
             items.append((new_key, v))
