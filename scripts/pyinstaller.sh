9c14b7423c46ea908c11bda78756095a14172375
Chris Glass <christopher.glass@vshn.net>
2020-10-23 13:28:59 +0200

    Fix binary build
    The pyinstaller binary builder expects some .so objcts to exist in the
    tree, but that should be moved (and bundled) by dependencies instead.

--- scripts/pyinstaller.sh
+++ scripts/pyinstaller.sh
@@ -14,8 +14,6 @@ pyi-makespec kapitan/"$entry".py --onefile \
     --add-data kapitan/reclass/reclass:reclass \
     --add-data kapitan/lib:kapitan/lib \
     --add-data kapitan/inputs/templates:kapitan/inputs/templates \
-    --add-data kapitan/inputs/helm/libtemplate.so:kapitan/inputs/helm \
-    --add-data kapitan/dependency_manager/helm/helm_fetch.so:kapitan/dependency_manager/helm \
     --hidden-import pyparsing --hidden-import jsonschema \
     --hidden-import 'pkg_resources.py2_warn' \
     --exclude-module doctest --exclude-module pydoc
