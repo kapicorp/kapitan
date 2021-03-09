be31e1cea49d1e1ed9cdeb6c15fad04d9803f8c7
Jean-Daniel Dupas <jddupas@xooloo.com>
2021-03-09 11:02:44 +0100

    Ignore helm stdout.

--- kapitan/helm_cli.py
+++ kapitan/helm_cli.py
@@ -1,10 +1,10 @@
 import subprocess
-from subprocess import PIPE
+from subprocess import PIPE, DEVNULL
 
 
 def helm_cli(args):
     try:
-        res = subprocess.run(args=["helm"] + args, stderr=PIPE)
+        res = subprocess.run(args=["helm"] + args, stderr=PIPE, stdout=DEVNULL)
         return res.stderr.decode() if res.returncode != 0 else ""
     except FileNotFoundError:
         return "helm binary not found. helm must be present in the PATH to use kapitan helm functionalities"
