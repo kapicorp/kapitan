f1f13442af44479b13cb1441a4c6027ce5cfbb96
Jean-Daniel Dupas <jddupas@xooloo.com>
2021-03-09 11:22:41 +0100

    [helm] log helm commands arguments.

--- kapitan/helm_cli.py
+++ kapitan/helm_cli.py
@@ -1,9 +1,13 @@
+import logging
 import subprocess
 from subprocess import PIPE, DEVNULL
+
+logger = logging.getLogger(__name__)
 
 
 def helm_cli(args):
     try:
+        logger.debug("launching helm with arguments: %s", args)
         res = subprocess.run(args=["helm"] + args, stderr=PIPE, stdout=DEVNULL)
         return res.stderr.decode() if res.returncode != 0 else ""
     except FileNotFoundError:
