847b84899e28aa524d1e6f46a84092aa1acbe0d5
Ricardo Amaro <ricardo.amaro@gmail.com>
2021-08-11 23:53:28 +0100

    fix pypi rc test 1 version (#771)

--- kapitan/version.py
+++ kapitan/version.py
@@ -8,7 +8,7 @@
 """Project description variables."""
 
 PROJECT_NAME = "kapitan"
-VERSION = "0.30.0rc0a"
+VERSION = "0.30.0rca0"
 DESCRIPTION = "Generic templated configuration management for Kubernetes, Terraform and other things"
 AUTHOR = "Ricardo Amaro"
 AUTHOR_EMAIL = "ramaro@kapicorp.com"
