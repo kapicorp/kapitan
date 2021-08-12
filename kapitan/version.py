c3818b13ee8731c41a223835e749fcdf199bd2df
Ricardo Amaro <ricardo.amaro@gmail.com>
2021-08-12 22:34:39 +0100

    yet another test release (#774)

--- kapitan/version.py
+++ kapitan/version.py
@@ -8,7 +8,7 @@
 """Project description variables."""
 
 PROJECT_NAME = "kapitan"
-VERSION = "0.30.0a0"
+VERSION = "0.30.0a1"
 DESCRIPTION = "Generic templated configuration management for Kubernetes, Terraform and other things"
 AUTHOR = "Ricardo Amaro"
 AUTHOR_EMAIL = "ramaro@kapicorp.com"
