6220e1b4102f3e21d2af93acbe336d178e464e92
serapch <kostovnedialko@gmail.com>
2022-03-18 13:01:43 +0200

    feature: add new lower_alpha_num ref function (#831)
    * feature: add new lower_alpha_num ref function
    
    * fix: rename ref function lower_alpha_num to loweralphanum
    
    Co-authored-by: Nedyalko Kostov <nedyalko.kostov@softwareag.com>
    Co-authored-by: Ricardo Amaro <ricardo.amaro@gmail.com>

--- tests/test_refs.py
+++ tests/test_refs.py
@@ -420,3 +420,23 @@ class Base64RefsTest(unittest.TestCase):
             raise Exception("ref is not sha256 hash")
 
     # TODO write tests for RefController errors (lookups, etc..)
+
+    def test_ref_function_loweralphanum(self):
+        "write loweralphanum to secret, confirm ref file exists, reveal and check"
+
+        tag = "?{plain:ref/loweralphanum||loweralphanum}"
+        REF_CONTROLLER[tag] = RefParams()
+        self.assertTrue(os.path.isfile(os.path.join(REFS_HOME, "ref/loweralphanum")))
+
+        file_with_tags = tempfile.mktemp()
+        with open(file_with_tags, "w") as fp:
+            fp.write("?{plain:ref/loweralphanum}")
+        revealed = REVEALER.reveal_raw_file(file_with_tags)
+        self.assertEqual(len(revealed), 8)  # default length of loweralphanum string is 8
+
+        # Test with parameter chars=16, correlating with string length 16
+        tag = "?{plain:ref/loweralphanum||loweralphanum:16}"
+        REF_CONTROLLER[tag] = RefParams()
+        REVEALER._reveal_tag_without_subvar.cache_clear()
+        revealed = REVEALER.reveal_raw_file(file_with_tags)
+        self.assertEqual(len(revealed), 16)
