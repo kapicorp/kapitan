f9d497f152d2e8d9be4a4cdf93f17b31d2f281cd
danny.heinrich <danny.heinrich@nexenio.com>
2022-07-06 21:14:09 +0200

    added SafeDumper to represent None type

--- kapitan/inputs/helm.py
+++ kapitan/inputs/helm.py
@@ -2,6 +2,7 @@ import logging
 import os
 import tempfile
 
+from yaml import SafeDumper
 import yaml
 
 from kapitan.errors import HelmTemplateError
@@ -33,7 +34,11 @@ class Helm(InputType):
             """dump helm values into a yaml file whose path will be passed over to Go helm code"""
             _, self.helm_values_file = tempfile.mkstemp(".helm_values.yml", text=True)
             with open(self.helm_values_file, "w") as fp:
-                yaml.safe_dump(args["helm_values"], fp)
+                SafeDumper.add_representer(
+                    type(None),
+                    lambda dumper, value: dumper.represent_scalar(u'tag:yaml.org,2002:null', '')
+                )
+                yaml.safe_dump(args["helm_values"], fp, default_flow_style=False)
 
         self.kube_version = None
         if "kube_version" in args:
