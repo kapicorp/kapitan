fc314f76b09921ddf636efa218db8cb4a96fabc6
Matteo Voges <98756476+MatteoVoges@users.noreply.github.com>
2023-05-11 13:44:22 +0200

    add resolving step for references in migration script

--- kapitan/migrate_omegaconf.py
+++ kapitan/migrate_omegaconf.py
@@ -2,31 +2,68 @@ import sys
 import os
 from ruamel.yaml import YAML
 from pathlib import Path
-import regex
+import regex as re
 
-REF_TOKEN = r".*\$\{.*\}.*"
+REF_TOKEN = r"\${([^\${}]*+(?:(?R)[^\${}]*)*+)}"
 
 
 def replace_token(token: str) -> str:
-    token = token.replace(":", ".")
 
-    return token.replace
+    inner_token = token[2:-1]
+
+    offset = 0
+    matches = re.finditer(REF_TOKEN, inner_token)
+    for match in matches:
+        replaced = replace_token(match.group())
+        inner_token = inner_token[: match.start() - offset] + replaced + inner_token[match.end() - offset :]
+        offset += len(match.group()) - len(replaced)
+
+    inner_token = inner_token.replace(":", ".")
+    inner_token = "parameters." + inner_token
+
+    token = "${" + inner_token + "}"
+
+    return token
 
 
 def replace_str(input: str) -> str:
-    if regex.match(REF_TOKEN, input):
-        return replace_token(input)
+
+    offset = 0
+    matches = re.finditer(REF_TOKEN, input)
+    for match in matches:
+        replaced = replace_token(match.group())
+        input = input[: match.start() - offset] + replaced + input[match.end() - offset :]
+        offset += len(match.group()) - len(replaced)
 
     return input
 
 
 def migrate_file(input_file: str) -> None:
     # load the file
-    yaml = YAML()
-    file_path = Path(input_file)
-    yaml.load(file_path)
+    yaml = YAML(typ="rt")
+    file_path = Path(input_file).resolve()
+    yaml_obj = yaml.load(file_path)
+    yaml.dump(yaml_obj, file_path)
 
-    # replace all references with modern
+    print(yaml_obj)
+    return
+
+    # replace all references with OmegaConf syntax
+    def migrate_yaml_obj(yaml_obj: dict | list | str) -> None:
+        # dictionary
+        if isinstance(yaml_obj, dict):
+            for k, v in yaml_obj.items():
+                yaml_obj[k] = migrate_yaml_obj(v)
+        # list
+        elif isinstance(yaml_obj, list):
+            yaml_obj = [migrate_yaml_obj(item) for item in yaml_obj]
+        # string --> replace the references
+        elif isinstance(yaml_obj, str):
+            yaml_obj = replace_str(yaml_obj)
+
+    yaml_obj = migrate_yaml_obj(yaml_obj)
+
+    yaml.dump(yaml_obj, file_path)
 
 
 def migrate(inv_path: str = "") -> None:
@@ -44,7 +81,9 @@ def migrate(inv_path: str = "") -> None:
 
     for root, subdirs, files in os.walk(targets_path):
         for target_file in files:
+            target_file = os.path.join(root, target_file)
             migrate_file(target_file)
+            return
 
     for root, subdirs, files in os.walk(classes_path):
         for class_file in files:
