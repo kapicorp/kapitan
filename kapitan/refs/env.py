c027542d8fb7ddbc58be21f7569b9cdec0bdc2a6
Matteo Voges <matteo.voges@nexenio.com>
2023-09-22 16:31:59 +0200

    feat: embedded env engine

--- kapitan/refs/env.py
+++ kapitan/refs/env.py
@@ -9,18 +9,18 @@ import base64
 import hashlib
 import os
 
-from kapitan.refs.base import PlainRef, PlainRefBackend
+from kapitan.refs.base64 import Base64Ref, Base64RefBackend
 
 DEFAULT_ENV_REF_VAR_PREFIX = "KAPITAN_VAR_"
 
 
-class EnvRef(PlainRef):
+class EnvRef(Base64Ref):
     def __init__(self, data, **kwargs):
         """
         writes plain data, which is the "default" value if the ref cannot be located in the KAPITAN_VAR_*
         environment vars prefix during the reveal phase.
         """
-        super().__init__(data, kwargs=kwargs)
+        super().__init__(data, from_base64=True, kwargs=kwargs)
         self.type_name = "env"
 
     def reveal(self):
@@ -31,15 +31,15 @@ class EnvRef(PlainRef):
         var_key = "{}{}".format(DEFAULT_ENV_REF_VAR_PREFIX, path_part)
         return os.getenv(var_key, default=os.getenv(var_key.upper(), default=self.data))
 
-    def compile(self):
-        """
-        Override the way an EnvRef is compiled, since we want to reveal it via the env later.
-        """
-        compiled = f"?{{{self.type_name}:{self.path}:{self.hash[:8]}}}"
-        return compiled
+    # def compile(self):
+    #     """
+    #     Override the way an EnvRef is compiled, since we want to reveal it via the env later.
+    #     """
+    #     compiled = f"?{{{self.type_name}:{self.path}:{self.hash[:8]}}}"
+    #     return compiled
 
 
-class EnvRefBackend(PlainRefBackend):
+class EnvRefBackend(Base64RefBackend):
     def __init__(self, path, ref_type=EnvRef, **ref_kwargs):
         "Get and create EnvRefs"
         super().__init__(path, ref_type, ref_kwargs=ref_kwargs)
