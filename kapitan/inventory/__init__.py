d655b12335201bb05c3f38bd5b54fb61ac162f29
Simon Gerber <simon.gerber@vshn.ch>
2024-02-26 14:14:22 +0100

    Implement inventory backend for reclass-rs
    We reuse `get_reclass_config()` in the `ReclassRsInventory` backend.
    Additionally, we make the backend selectable through the
    `--inventory-backend` CLI option.

--- kapitan/inventory/__init__.py
+++ kapitan/inventory/__init__.py
@@ -1,10 +1,12 @@
 from typing import Type
 
 from .inv_reclass import ReclassInventory
+from .inv_reclass_rs import ReclassRsInventory
 from .inventory import Inventory
 
 # Dict mapping values for command line flag `--inventory-backend` to the
 # associated `Inventory` subclass.
 AVAILABLE_BACKENDS: dict[str, Type[Inventory]] = {
     "reclass": ReclassInventory,
+    "reclass-rs": ReclassRsInventory,
 }
