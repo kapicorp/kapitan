9dc7d1e8f53136fcc9d628a2a98dc4bbbf31502f
Alessandro De Maria <ademaria@isomorphiclabs.com>
2024-12-17 13:40:51 +0000

    Final fixes

--- kapitan/initialiser.py
+++ kapitan/initialiser.py
@@ -34,5 +34,22 @@ def initialise_skeleton(args):
     if set(glob.iglob(os.path.join(directory, "./*"))):
         raise KapitanError(f"Directory {directory} is not empty. Please initialise in an empty directory.")
 
-    logger.info(f"Initialising skeleton from {template_git_url}@{checkout_ref} in {directory}")
-    run_copy(template_git_url, vcs_ref=args.checkout_ref, unsafe=True, dst_path=directory)
+    logger.info(f"Initialising kapitan from {template_git_url}@{checkout_ref} in {directory}")
+    user_defaults = {
+        "kapitan_version": kapitan_version,
+    }
+    run_copy(
+        template_git_url,
+        vcs_ref=args.checkout_ref,
+        unsafe=True,
+        dst_path=directory,
+        quiet=True,
+        user_defaults=user_defaults,
+        skip_answered=False,
+    )
+
+    if directory == os.path.abspath(os.path.curdir):
+        logger.info(f"Successfully initialised: run `kapitan --version`")
+    else:
+        logger.info(f"Successfully initialised kapitan in {directory}")
+        logger.info("Please go to the directory and run `kapitan --version`")
