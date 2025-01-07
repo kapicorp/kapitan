4b05008038e8d847554e1a3a37c6c2e88b2f8b3c
Alessandro De Maria <ademaria@isomorphiclabs.com>
2025-01-07 12:50:31 +0000

    Kapitan release 0.34.4

--- kapitan/version.py
+++ kapitan/version.py
@@ -8,7 +8,7 @@
 """Project description variables."""
 
 PROJECT_NAME = "kapitan"
-VERSION = "0.34.3"
+VERSION = "0.34.4"
 DESCRIPTION = "Generic templated configuration management for Kubernetes, Terraform and other things"
 AUTHOR = "Ricardo Amaro"
 AUTHOR_EMAIL = "ramaro@kapicorp.com"
