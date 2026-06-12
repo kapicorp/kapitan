kapicorp/kapitan

commit 65e98654a62eefc4b5bef1c0b6ac7f23289aa0f6

Implement kadet input type cache (#1319)
Fixes #1318 

## Proposed Changes

* Introduces a generic caching mechanism for Kapitan inputs. Its main
purpose is to store and retrieve compiled objects to avoid re-compiling
them on every run, thus improving performance.
* As part of the caching mechanism via the InputCache class, a
file-based cache is available as a content addressable storage mechanism
with the intention of being generic and good enough for other input
types.
* InputCache assumes all compiled objects are "pickleable" as the
implementation uses Python's pickle library.
* Removes the old cache implementation as it was not tested, valid nor
maintained.
* The cache is only enabled when the `--cache` / `-c` flags are set in
the `compile` command.

### Notes
* Compiled objects will still be inspected at compile time to reveal
references
* This implementation assumes that either `XDG_CACHE_HOME` or `HOME` env
vars are set as the cache home so that the cache artifacts can be
written to `"{cache_home}/.cache/kapitan/{input_type_name}"`

## Docs and Tests

* [x] Tests added
* [ ] Updated documentation

---------

Co-authored-by: Ricardo Amaro <ramaro@phaidra.ai>

kapitan/inputs/kadet.py
@@ -7,11 +7,13 @@
 
 import contextvars
 import inspect
+import json
 import logging
 import os
 import sys
 from functools import cache
 from importlib.util import module_from_spec, spec_from_file_location
+from pathlib import Path
 
 import kadet
 from kadet import BaseModel, BaseObj, Dict
@@ -19,6 +21,7 @@ from kadet import BaseModel, BaseObj, Dict
 from kapitan import cached
 from kapitan.errors import CompileError
 from kapitan.inputs.base import InputType
+from kapitan.inputs.cache import InputCache
 from kapitan.inventory.model.input_types import KapitanInputTypeKadetConfig
 
 
@@ -40,6 +43,20 @@ def inventory_global(lazy=False):
 
 def inventory(lazy=False):
     return inventory_global(lazy)[current_target.get()]
+
+
+def inventory_frozen():
+    return kadet.Box(data=inventory().dump(), frozen_box=True)
+
+
+@cache
+def inventory_digest(target_name):
+    # XXX target_name parameter is only used for the LRU cache
+    h = InputCache.hash_object()
+    h.update(
+        json.dumps(inventory_frozen(), sort_keys=True, default=str).encode("utf-8")
+    )
+    return h.digest()
 
 
 def module_from_path(path, check_name=None):
@@ -91,49 +108,57 @@ class Kadet(InputType):
         Compile a kadet input file.
 
         Write the kadet evaluated items as files to compile_path.  External variables are not used in Kadet.
-
-        kwargs:
-            output (str): default 'yaml', accepts 'json', 'toml', 'plain'
-            prune_output (bool): default False, prune empty dictionaries and lists from output
-            reveal (bool): default False, set to True to reveal refs on compile
-            target_name (str): default None, set to the current target being compiled
-            indent (int): default 2, indentation level for yaml/json output
         """
 
         input_params = config.input_params
         target_name = self.target_name
         current_target.set(target_name)
         search_paths.set(self.search_paths)
-
-        # set compile_path allowing kadet functions to have context on where files
-        # are being compiled on the current kapitan run
-        # we only do this if user didn't pass its own value
-        input_params.setdefault("compile_path", compile_path)
-
-        kadet_module, spec = module_from_path(input_path)
-        sys.modules[spec.name] = kadet_module
-        spec.loader.exec_module(kadet_module)
-        logger.debug("Kadet.compile_file: spec.name: %s", spec.name)
-
-        kadet_arg_spec = inspect.getfullargspec(kadet_module.main)
-        logger.debug("Kadet main args: %s", kadet_arg_spec.args)
-
-        if len(kadet_arg_spec.args) > 1:
-            raise ValueError(f"Kadet {spec.name} main parameters not equal to 1 or 0")
-
+        inputs_hash = None
         output_obj = None
-        try:
-            if len(kadet_arg_spec.args) == 1:
-                output_obj = kadet_module.main(input_params)
-            elif len(kadet_arg_spec.args) == 0:
-                output_obj = kadet_module.main()
 
-        except Exception as exc:
-            raise CompileError(
-                f"Could not load Kadet module: {spec.name[16:]}"
-            ) from exc
+        if cache_obj := self.cacheable():
+            inputs_hash = self.inputs_hash(
+                inventory_digest(current_target.get()),
+                target_name,
+                Path(input_path),
+            )
 
-        output_obj = _to_dict(output_obj)
+            output_obj = cache_obj.get(inputs_hash)
+
+        if output_obj is None:
+            # set compile_path allowing kadet functions to have context on where files
+            # are being compiled on the current kapitan run
+            # we only do this if user didn't pass its own value
+            input_params.setdefault("compile_path", compile_path)
+
+            kadet_module, spec = module_from_path(input_path)
+            sys.modules[spec.name] = kadet_module
+            spec.loader.exec_module(kadet_module)
+            logger.debug("Kadet.compile_file: spec.name: %s", spec.name)
+            kadet_arg_spec = inspect.getfullargspec(kadet_module.main)
+            logger.debug("Kadet main args: %s", kadet_arg_spec.args)
+
+            if len(kadet_arg_spec.args) > 1:
+                raise ValueError(
+                    f"Kadet {spec.name} main parameters not equal to 1 or 0"
+                )
+
+            try:
+                if len(kadet_arg_spec.args) == 1:
+                    output_obj = kadet_module.main(input_params)
+                elif len(kadet_arg_spec.args) == 0:
+                    output_obj = kadet_module.main()
+
+            except Exception as exc:
+                raise CompileError(
+                    f"Could not load Kadet module: {spec.name[16:]}"
+                ) from exc
+
+            output_obj = _to_dict(output_obj)
+
+            if cache_obj := self.cacheable():
+                cache_obj.set(inputs_hash, output_obj)
 
         # Return None if output_obj has no output
         if not output_obj:
@@ -142,6 +167,143 @@ class Kadet(InputType):
         for item_key, item_value in output_obj.items():
             file_path = os.path.join(compile_path, item_key)
             self.to_file(config, file_path, item_value)
+
+    def inputs_hash(self, *inputs):
+        """
+        Calculates a deterministic hash for a variable set of inputs.
+
+        This function return a consistent hash regardless of the
+        order of the input arguments. It groups inputs by type (dict, list,
+        str, int, bytes, Path), sorts them within their groups, and then
+        calculates a final hash from the combined hashes of each group.
+
+        - Dictionaries and lists are serialized to JSON with sorted keys.
+        - Paths are recursively hashed using `walk_and_hash`.
+        - All other types are converted to bytes and hashed.
+
+        Args:
+            *inputs: A variable number of inputs to be hashed.
+
+        Returns:
+            A hex digest string representing the deterministic hash of the inputs.
+        """
+
+        # Temporary lists for sortable representations of inputs
+        dict_reps = []
+        list_reps = []
+        str_reps = []
+        int_reps = []
+        bytes_reps = []
+        path_reps = []
+
+        for i in inputs:
+            if isinstance(i, dict):
+                dict_reps.append(json.dumps(i, sort_keys=True, default=str))
+            elif isinstance(i, list):
+                list_reps.append(json.dumps(i, sort_keys=True, default=str))
+            elif isinstance(i, str):
+                str_reps.append(i)
+            elif isinstance(i, int):
+                int_reps.append(i)
+            elif isinstance(i, bytes):
+                bytes_reps.append(i)
+            elif isinstance(i, Path):
+                path_reps.append(i)
+
+        # Sort the representations
+        dict_reps.sort()
+        list_reps.sort()
+        str_reps.sort()
+        int_reps.sort()
+        bytes_reps.sort()
+        path_reps.sort(key=str)  # Sort paths by their string representation
+
+        # Hash the sorted representations
+        h_dicts = InputCache.hash_object()
+        for rep in dict_reps:
+            h_dicts.update(rep.encode("utf-8"))
+
+        h_lists = InputCache.hash_object()
+        for rep in list_reps:
+            h_lists.update(rep.encode("utf-8"))
+
+        h_strs = InputCache.hash_object()
+        for rep in str_reps:
+            h_strs.update(rep.encode("utf-8"))
+
+        h_ints = InputCache.hash_object()
+        for rep in int_reps:
+            h_ints.update(bytes(rep))
+
+        h_bytes = InputCache.hash_object()
+        for rep in bytes_reps:
+            h_bytes.update(rep)
+
+        h_paths = InputCache.hash_object()
+        for rep in path_reps:
+            walk_and_hash(rep, self.cacheable(), h_paths)
+
+        h_final = InputCache.hash_object()
+        h_final.update(
+            h_dicts.digest()
+            + h_lists.digest()
+            + h_strs.digest()
+            + h_ints.digest()
+            + h_bytes.digest()
+            + h_paths.digest()
+        )
+
+        return h_final.hexdigest()
+
+    def cacheable(self):
+        if cached.args.cache:
+            if cached.kapitan_input_kadet is None:
+                cached.kapitan_input_kadet = InputCache("kadet")
+
+            return cached.kapitan_input_kadet
+        return False
+
+
+def walk_and_hash(path: Path, input_cache: InputCache, path_hash):
+    """
+    Recursively walk a path and update a hash object with the contents of all files.
+    This implementation is deterministic.
+    """
+    if not path.exists() or str(path).endswith("__pycache__"):
+        return
+
+    if path.is_file():
+        if cached_hash_digest := get_path_hash_from_input_kv(path, input_cache):
+            path_hash.update(cached_hash_digest)
+            logger.debug(
+                "KV Memory hit for path: %s, digest: %s", path, path_hash.hexdigest()
+            )
+            return
+
+        with open(path, "rb") as fp:
+            file_hash = InputCache.hash_file_digest(fp)
+            digest = file_hash.digest()
+            set_path_hash_input_kv(path, digest, input_cache)
+            path_hash.update(digest)
+
+    elif path.is_dir():
+        for item in sorted(path.iterdir(), key=lambda p: p.name):
+            walk_and_hash(item, input_cache, path_hash)
+
+
+def get_path_hash_from_input_kv(path: Path, input_cache: InputCache):
+    try:
+        # TODO temp hack to avoid input_cache being False
+        if input_cache:
+            return input_cache.kv_cache[str(path)]
+    except KeyError:
+        return None
+
+
+def set_path_hash_input_kv(path: Path, h_file, input_cache: InputCache):
+    # TODO temp hack to avoid input_cache being False
+    if input_cache:
+        input_cache.kv_cache[str(path)] = h_file
 
 
 def _to_dict(obj):

bin/kapitan
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-BINPATH=`dirname $0`
-PYTHONPATH="$BINPATH/../:$PYTHONPATH" python3 -m kapitan $@
+BINPATH=$(dirname "$0")
+PYTHONPATH="$BINPATH/../:$PYTHONPATH" python3 -m kapitan "$@"

default.nix
@@ -1,0 +1,29 @@
+# from https://nixos.wiki/wiki/Python#Emulating_virtualenv_with_nix-shell
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  buildInputs = [
+    pkgs.act
+    pkgs.gnumake
+    pkgs.kubernetes-helm
+    pkgs.libffi
+    pkgs.podman
+    pkgs.poetry
+    pkgs.ripgrep
+    pkgs.uv
+
+    pkgs.python312
+    pkgs.python312.pkgs.black
+    pkgs.python312.pkgs.cffi
+    pkgs.python312.pkgs.pip
+    pkgs.python312.pkgs.setuptools
+  ];
+  shellHook = ''
+    # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+    # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+    export PIP_PREFIX=$(pwd)/_build/pip_packages
+    export PYTHONPATH="$PIP_PREFIX/${pkgs.python312.sitePackages}:$PYTHONPATH"
+    export PATH="$PIP_PREFIX/bin:$PATH"
+    unset SOURCE_DATE_EPOCH
+  '';
+}

kapitan/cached.py
@@ -35,9 +35,11 @@ revealer_obj: Any = None
 args: Namespace = Namespace()  # args won't need resetting
 inv_sources: set[str] = set()
 
+# Kadet caches
+kapitan_input_kadet = None
 
-def reset_cache() -> None:
-    """Reset all cache variables to their initial state."""
+
+def reset_cache():
     global \
         inv, \
         global_inv, \

kapitan/cli.py
@@ -268,7 +268,6 @@ def build_parser():
         action="store_true",
         default=from_dot_kapitan("compile", "embed-refs", False),
     )
-
     compile_parser.add_argument(
         "--inventory-path",
         default=from_dot_kapitan("compile", "inventory-path", "./inventory"),
@@ -283,18 +282,9 @@ def build_parser():
     compile_parser.add_argument(
         "--cache",
         "-c",
-        help="enable compilation caching to .kapitan_cache\
-        and dependency caching to .dependency_cache, default is False",
+        help="enable compilation caching to $XDG_CACHE_HOME/kapitan, default is False [this is EXPERIMENTAL]",
         action="store_true",
         default=from_dot_kapitan("compile", "cache", False),
-    )
-    compile_parser.add_argument(
-        "--cache-paths",
-        type=str,
-        nargs="+",
-        default=from_dot_kapitan("compile", "cache-paths", []),
-        metavar="PATH",
-        help="cache additional paths to .kapitan_cache, default is []",
     )
     compile_parser.add_argument(
         "--ignore-version-check",

kapitan/inputs/base.py
@@ -201,6 +201,13 @@ class InputType:
         """
         return NotImplementedError
 
+    @abc.abstractmethod
+    def inputs_hash(self, *inputs, **kwargs):
+        return NotImplementedError
+
+    def cacheable(self) -> bool:
+        return False
+
 
 class CompilingFile:
     """

kapitan/inputs/cache.py
@@ -1,0 +1,100 @@
+import hashlib
+import logging
+import os
+import pickle
+from pathlib import Path
+from typing import Tuple
+
+from kapitan.errors import CompileError
+
+
+logger = logging.getLogger(__name__)
+
+
+class InputCache:
+    def __init__(self, input_type_name: str):
+        if cache_home := os.environ.get("XDG_CACHE_HOME"):
+            self.input_cache_home = f"{cache_home}/kapitan/{input_type_name}"
+        elif cache_home := os.environ.get("HOME"):
+            self.input_cache_home = f"{cache_home}/.cache/kapitan/{input_type_name}"
+        else:
+            raise CompileError(
+                "Could not get cache dir: $XDG_CACHE_HOME or $HOME not set."
+            )
+
+        self.kv_cache = {}
+
+        logger.debug("Input cache home: %s", self.input_cache_home)
+
+    def hash_paths(self, inputs_hash) -> Tuple[Path, Path, Path]:
+        sub_path = Path(self.input_cache_home, inputs_hash[:2])
+        cached_path = Path(sub_path, inputs_hash[2:])
+        cached_path_lock = Path(str(cached_path) + ".lock")
+        return cached_path, cached_path_lock, sub_path
+
+    def get(self, inputs_hash, lock_retries=2) -> dict | None:  # output_obj
+        cached_path, cached_path_lock, _ = self.hash_paths(inputs_hash)
+        if not cached_path_lock.exists():
+            for retry in range(lock_retries):
+                try:
+                    with open(cached_path, "rb") as fp:
+                        logger.debug(
+                            "Loading cache hit: %s (try %d/%d)",
+                            cached_path,
+                            retry,
+                            lock_retries,
+                        )
+                        return self.load_output(fp)
+                except FileNotFoundError:
+                    pass
+        return None
+
+    def set(self, inputs_hash, output_obj, lock_retries=2):
+        cached_path, cached_path_lock, sub_path = self.hash_paths(inputs_hash)
+        for retry in range(lock_retries):
+            # dont write if already exists
+            if cached_path.exists():
+                return inputs_hash
+
+            if not cached_path_lock.exists():
+                sub_path.mkdir(parents=True, exist_ok=True)
+                with open(cached_path_lock, "wb") as fp:
+                    logger.debug(
+                        "Writing cache file: %s (try %d/%d)",
+                        cached_path_lock,
+                        retry,
+                        lock_retries,
+                    )
+                    self.dump_output(output_obj, fp)
+                cached_path_lock.rename(
+                    Path(str(cached_path_lock).removesuffix(".lock"))
+                )
+                logger.debug(
+                    "Moved cache lock to: %s (try %d/%d)",
+                    cached_path,
+                    retry,
+                    lock_retries,
+                )
+
+                return inputs_hash
+        return None
+
+    def set_value(self, key, value):
+        self.kv_cache[key] = value
+
+    def get_key(self, key):
+        return self.kv_cache[key]
+
+    @staticmethod
+    def hash_object():
+        return hashlib.blake2b(digest_size=32)
+
+    @staticmethod
+    def hash_file_digest(fp):
+        return hashlib.file_digest(fp, "blake2b")
+
+    def dump_output(self, output_obj, fp):
+        return pickle.dump(output_obj, fp)
+
+    def load_output(self, fp):
+        return pickle.load(fp)

kapitan/inventory/model/__init__.py
@@ -9,16 +9,16 @@ from kapitan.inventory.model.references import KapitanReferenceConfig
 
 class KapitanEssentialVars(BaseModel):
     model_config = ConfigDict(extra="allow")
-    target: Optional[str] = None
+    target: str | None = None
 
 
 class KapitanInventorySettings(BaseModel):
     compile: list[CompileInputTypeConfig] = []
     vars: KapitanEssentialVars = KapitanEssentialVars()
     labels: dict[str, str] = {}
-    dependencies: Optional[list[DependencyTypeConfig]] = []
+    dependencies: list[DependencyTypeConfig] | None = []
     target_full_path: str = ""
-    secrets: Optional[KapitanReferenceConfig] = None
+    secrets: KapitanReferenceConfig | None = None
     validate_: list[dict] = Field(alias="validate", default=[])
 
 
@@ -31,16 +31,16 @@ class KapitanMetadataName(BaseModel):
 
 class KapitanInventoryMetadata(BaseModel):
     model_config = ConfigDict(extra="allow", validate_assignment=True)
-    name: Optional[KapitanMetadataName] = None
+    name: KapitanMetadataName | None = None
 
 
 class KapitanInventoryParameters(BaseModel):
     model_config = ConfigDict(extra="allow")
 
-    kapitan: Optional[KapitanInventorySettings] = KapitanInventorySettings()
-    kapitan_metadata: Optional[KapitanInventoryMetadata] = Field(
+    kapitan: KapitanInventorySettings | None = KapitanInventorySettings()
+    kapitan_metadata: KapitanInventoryMetadata | None = Field(
         alias="_kapitan_", default=None
     )
-    reclass_metadata: Optional[KapitanInventoryMetadata] = Field(
+    reclass_metadata: KapitanInventoryMetadata | None = Field(
         alias="_reclass_", default=None
     )

commit 46499d2b6ab2d658a5303c8bf09b42ea21350f71

Fix Kadet cache key collisions from input_params (#1520)
## Summary

- Include Kadet's effective `input_params` in the compilation cache key.
- Build those params from a local copy so `compile_file()` no longer
mutates `config.input_params` when adding the default `compile_path`.
- Add a focused unit test that asserts `Kadet.compile_file()` passes the
effective params into `inputs_hash()` and leaves the original config
params unchanged.

## Bug

Kadet cache keys currently include the target inventory digest, target
name, and Kadet input path, but not the per-input parameters passed to
the Kadet component.

When `kapitan compile --cache` is used, two compile entries can call the
same Kadet component with different `input_params`. Without those params
in the key, both entries compute the same cache key, so the second call
can load and write the first call's cached output. That makes
parameterized Kadet components produce stale or incorrect manifests
whenever the same component is reused with different params.

`compile_file()` also adds a default `compile_path` to the params passed
into Kadet modules. This PR treats that effective params dict as part of
the cache input, while avoiding mutation of the original
`config.input_params` object.

## Root cause

The regression was introduced by
`65e98654a62eefc4b5bef1c0b6ac7f23289aa0f6` (`Implement kadet input type
cache (#1319)`). That commit added Kadet cache lookup and
`inputs_hash()`, but the key only used:

- `inventory_digest(current_target.get())`
- `target_name`
- `Path(input_path)`

Kadet `input_params` already existed before that change, so enabling the
new cache created a collision path for parameterized Kadet inputs.

## Tests

- `uv run pytest -q tests/test_kadet.py --no-cov`
- `uv run pytest -q tests/test_compile.py::CompileTestResourcesTestKadet
--no-cov`
- `uv run ruff check kapitan/inputs/kadet.py tests/test_kadet.py`

Co-authored-by: Ricardo Amaro <ricardo.amaro@gmail.com>

kapitan/inputs/kadet.py
@@ -128,28 +128,29 @@ class Kadet(InputType):
         Write the kadet evaluated items as files to compile_path.  External variables are not used in Kadet.
         """
 
-        input_params = config.input_params
+        input_params = dict(config.input_params)
         target_name = self.target_name
         current_target.set(target_name)
         search_paths.set(self.search_paths)
         inputs_hash = None
         output_obj = None
 
+        # set compile_path allowing kadet functions to have context on where files
+        # are being compiled on the current kapitan run
+        # we only do this if user didn't pass its own value
+        input_params.setdefault("compile_path", compile_path)
+
         if cache_obj := self.cacheable():
             inputs_hash = self.inputs_hash(
                 inventory_digest(current_target.get()),
                 target_name,
                 Path(input_path),
+                input_params,
             )
 
             output_obj = cache_obj.get(inputs_hash)
 
         if output_obj is None:
-            # set compile_path allowing kadet functions to have context on where files
-            # are being compiled on the current kapitan run
-            # we only do this if user didn't pass its own value
-            input_params.setdefault("compile_path", compile_path)
-
             kadet_module, spec = module_from_path(input_path)
             sys.modules[spec.name] = kadet_module
             spec.loader.exec_module(kadet_module)

tests/test_kadet.py
@@ -9,8 +9,15 @@
 
 import tempfile
 import unittest
+from argparse import Namespace
+from pathlib import Path
+from unittest import mock
 
-from kadet import ABORT_EXCEPTION_TYPE, BaseObj, Dict
+import kadet
+from kadet import BaseObj, Dict
+
+from kapitan.inputs.kadet import Kadet
+from kapitan.inventory.model.input_types import KapitanInputTypeKadetConfig
 
 
 class KadetTestObj(BaseObj):
@@ -117,7 +124,7 @@ class KadetTest(unittest.TestCase):
         self.assertEqual(output, desired_output)
 
     def test_need(self):
-        with self.assertRaises(ABORT_EXCEPTION_TYPE):
+        with self.assertRaises(kadet.ABORT_EXCEPTION_TYPE):
             KadetTestObj(this_should_error=True)
 
     def test_update_root_yaml(self):
@@ -165,3 +172,43 @@ class KadetTest(unittest.TestCase):
         output = kobj.dump()
         desired_output = {"this": "that", "list": [1, 2, 3]}
         self.assertEqual(output, desired_output)
+
+
+class KadetCompileCacheTest(unittest.TestCase):
+    def test_compile_cache_key_includes_input_params(self):
+        input_params = {"value": "second"}
+        config = KapitanInputTypeKadetConfig(
+            input_paths=["component"],
+            output_path=".",
+            input_params=input_params,
+        )
+        compiler = Kadet(
+            "compiled",
+            ["."],
+            None,
+            "test-target",
+            Namespace(cache=True),
+        )
+
+        cache = mock.Mock()
+        cache.get.return_value = {}
+
+        with (
+            mock.patch.object(compiler, "cacheable", return_value=cache),
+            mock.patch.object(
+                compiler, "inputs_hash", return_value="cache-key"
+            ) as inputs_hash,
+            mock.patch(
+                "kapitan.inputs.kadet.inventory_digest", return_value=b"inventory"
+            ),
+        ):
+            compiler.compile_file(config, "component", "compiled/test-target")
+
+        inputs_hash.assert_called_once_with(
+            b"inventory",
+            "test-target",
+            Path("component"),
+            {"value": "second", "compile_path": "compiled/test-target"},
+        )
+        cache.get.assert_called_once_with("cache-key")
+        self.assertEqual(config.input_params, input_params)